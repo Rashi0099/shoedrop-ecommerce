fd7ab9d015d2c6d06362805ba963fb31a63f3609
Rashi0099 <abdulrasheed5140@gmail.com>
2026-08-13 20:32:47 +0530

    setted debug false

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,6 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = [
